4daad7752033d57b243bc7cf2d5a6e98e835a38f
mihir-jain <mihirrjain200@gmail.com>
2021-07-03 11:53:59 -0700

    Commit Message

---
+++ app/src/main/java/com/example/friendzone/ui/chat/ChatFragment.java
@@ -0,0 +1,40 @@
+package com.example.friendzone.ui.chat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.friendzone.databinding.FragmentChatBinding;
+
+public class ChatFragment extends Fragment {
+
+    private ChatViewModel dashboardViewModel;
+    private FragmentChatBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                new ViewModelProvider(this).get(ChatViewModel.class);
+
+        binding = FragmentChatBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
